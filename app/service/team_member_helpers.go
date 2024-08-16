dbb320a10bb19f8abdae8966de4f9b5a27d12a36
Adam Nasrudin <adamnasrudin858@gmail.com>
2024-08-16 20:17:18 +0700

    update pointer impl struct

--- app/service/team_member_helpers.go
+++ app/service/team_member_helpers.go
@@ -7,7 +7,7 @@ import (
 	"github.com/adamnasrudin03/go-skeleton-gin/app/dto"
 )
 
-func (s TeamMemberSrv) checkDuplicate(ctx context.Context, req dto.TeamMemberDetailReq) error {
+func (s *TeamMemberSrv) checkDuplicate(ctx context.Context, req dto.TeamMemberDetailReq) error {
 	var (
 		opName = "TeamMemberService-checkDuplicate"
 		err    error
